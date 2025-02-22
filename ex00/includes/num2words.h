7f35b1b6560c57f9cbc01fd6b55a607a350266dc
Lucas Correia Dupuy <lucorrei@fu-r6-p1.s19.be>
2025-02-22 16:10:33 +0100

    small fixes, write mags will need testing but the logic is solid i think (i am not a trustworthy source on logic)

--- ex00/includes/num2words.h
+++ ex00/includes/num2words.h
@@ -6,7 +6,6 @@
 # include <stdbool.h>
 # include <unistd.h>
 # include <fcntl.h>
-# include <stdlib.h>
 
 typedef struct s_dictionary
 {
@@ -14,11 +13,11 @@ typedef struct s_dictionary
 	char	**words;
 }	t_dictionary;
 
-int				ft_atoi(char *str);
-void			ft_putstr(char *str);
-t_dictionary	*get_dictionary(char *dict_path);
+int	ft_atoi(char *str);
+void	ft_putstr(char *str);
+t_dictionary *get_dictionary(char *dict_path);
 
-# define PRINT_GOT_ERROR -1;
+# define PRINT_START_STOP -1;
 # define PRINT_HORIZONTAL_LINE -2;
 # define PRINT_COMMA -3;
 # define PRINT_AND -4;
--- ex00/srcs/write_order_of_magnitude.c
+++ ex00/srcs/write_order_of_magnitude.c
@@ -6,10 +6,46 @@
 /*   By: arcornil <arcornil@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 11:14:28 by lucorrei          #+#    #+#             */
-/*   Updated: 2025/02/22 12:41:53 by lucorrei         ###   ########.fr       */
+/*   Updated: 2025/02/22 15:33:43 by lucorrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/num2words.h"
+
+bool	try_full_nums(const t_dictionary dict, char *trying, int *printer, int len)
+{	
+	int	i;
+
+	i = -1;
+	if (len == 0)
+		return (true);
+	if (*trying == 0)
+		return (false);
+	while (dict.nums[++i])
+	{
+		if (ft_strlen(dict.nums[i] == len && dict.nums[i][0] == *trying))
+		{
+			if (is_zero_padded(dict.nums[i]))
+			{
+				*printer++ = i;
+				if (!try_full_nums(dict, trying + 1, printer, len - 1))
+					break ;
+			}
+		}
+	}
+}
+bool	write_units(const t_dictionary dict, char *to_write, int *printer)
+{
+	int	i;
+	while (dict.nums[++i])
+	{
+		if ft_strlen(dict.nums[i] == 1 && dict.nums[i][0] == *to_write)
+		{
+			*printer = i;
+			return (true);
+		}
+	}
+	return (false);
+}
 
 bool	write_tens(const t_dictionary dict, char *to_write, int *printer)
 {
@@ -18,25 +54,22 @@ bool	write_tens(const t_dictionary dict, char *to_write, int *printer)
 	i = -1;
 	while (dict.nums[++i])
 	{
-		if (ft_strlen(dict.nums[i]) == 2)
+		if (ft_strlen(dict.nums[i]) == 2 && dict.nums[i][0] == *to_write)
 		{
-			if (dict.nums[i][0] == *to_write)
+			if (*to_write >= '2')
 			{
-				if (*to_write >= '2')
-				{
-					ft_putstr(dict.words[i]);
-					write(1, "-", 1);
-					return (write_units(to_write + 1));
-				}
-				else if (dict.nums[i][1] == *(to_write + 1))
-				{
-					ft_putstr(dict.words[i]);
-					return (true);
-				}
+				*printer++ = i;
+				*printer++ = PRINT_HORIZONTAL_LINE;
+				return (write_units(dict, to_write + 1, printer));
+			}
+			else if (dict.nums[i][1] == *(to_write + 1))
+			{
+				printer = i;
+				return (true);
 			}
 		}
 	}
-	return (false);
+	return (try_full_nums(dict, to_write, printer, 2));
 }
 
 bool	write_hundreds(t_dictionary dict, char *to_write, int *printer)
@@ -45,18 +78,23 @@ bool	write_hundreds(t_dictionary dict, char *to_write, int *printer)
 	bool	success;
 
 	i = -1;
+	if (*to_write = '0')
+	{
+		*printer = PRINT_AND;
+		return write_tens(dict, to_write + 1, printer + 1);
+	}
 	while (dict_nums[++i])
 	{
 		if (ft_strlen(dict.nums[i] == 3))
 		{
 			if (is_magnitude(dict.nums[i]))
 			{
-				if (!write_units(to_write, printer))
+				if (!write_units(dict, to_write, printer))
 					return (false);
-				++printer = i;
+				*(++printer) = i;
+				return (write_tens(dict, to_write + 1, printer + 1));
 			}
 		}
 	}
-	if (!write_tens(dict, to_write + 1, printer + 1))
-		return (false);
+	return (try_full_nums(dict, to_write, printer, 3));
 }
